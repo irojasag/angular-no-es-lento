9c10c050736e96a229b76890d8d4696042d096de
Ignacio Rojas Aguilar <imoon521@gmail.com>
2021-03-06 00:35:20 -0600

    Add about

--- primer-carga-lenta/src/app/app-routing.module.ts
+++ primer-carga-lenta/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LayoutComponent } from './layout/layout.component';
 
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
       },
       {
         path: '**',
--- primer-carga-lenta/src/app/app.module.ts
+++ primer-carga-lenta/src/app/app.module.ts
@@ -11,9 +11,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardLocationComponent } from './dashboard-location/dashboard-location.component';
 import { DashboardCharactersComponent } from './dashboard-characters/dashboard-characters.component';
 import { DashboardEpisodesComponent } from './dashboard-episodes/dashboard-episodes.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent, DashboardComponent, DashboardLocationComponent, DashboardCharactersComponent, DashboardEpisodesComponent],
+  declarations: [AppComponent, LayoutComponent, DashboardComponent, DashboardLocationComponent, DashboardCharactersComponent, DashboardEpisodesComponent, ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
